527421cde03696613926f58d93c81df5107cbc9f
Jullyana <jullyana@vtex.com.br>
2018-09-25 11:10:21 -0300

    Update ESLint on index

--- react/index.js
+++ react/index.js
@@ -1,31 +1,26 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {compose, graphql} from 'react-apollo'
+import { compose, graphql } from 'react-apollo'
 
 import getCampaigns from './graphql/getCampaigns.graphql'
 
 class Campaigns extends Component {
   render() {
-
-    return (
-      <div>
-        Hello world
-      </div>
-    )
+    return <div>Hello world</div>
   }
 }
 
 Campaigns.propTypes = {
   data: PropTypes.object,
-  mutate: PropTypes.func
+  mutate: PropTypes.func,
 }
 
-//export default Campaigns
+// export default Campaigns
 export default compose(
   graphql(getCampaigns, {
-    options: ({ params }) => ({
+    options: () => ({
       ssr: false,
-      variables: { conditionType: "campaigns" },
+      variables: { conditionType: 'campaigns' },
     }),
   })
 )(Campaigns)
