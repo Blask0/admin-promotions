5c03420014082a7b2766d7fc7c58f43cc1c23df8
Jullyana Vycas <jullyana@vtex.com.br>
2018-10-16 17:29:25 -0300

    Add general panel to campaign creation

--- react/create-campaign.js
+++ react/create-campaign.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import { PageHeader } from 'vtex.styleguide'
+import { Box, Input, PageHeader, Textarea, Toggle } from 'vtex.styleguide'
 
 import './global.css'
 
@@ -15,6 +15,13 @@ class CreateCampaign extends Component {
   componentDidMount = () => {
     window.postMessage({ action: { type: 'STOP_LOADING' } }, '*')
   }
+
+  static Panel = props => (
+    <Box>
+      <h1 className="f3 normal ma0">{props.title}</h1>
+      <div className="mt7">{props.children}</div>
+    </Box>
+  )
 
   render() {
     const { navigate } = this.context
@@ -31,7 +38,19 @@ class CreateCampaign extends Component {
         >
           <SaveCampaignButton />
         </PageHeader>
-        <div className="ph7" />
+        <div className="ph7">
+          <CreateCampaign.Panel title="General">
+            <div style={{ maxWidth: 500 }}>
+              <Input label="Name" size="large" />
+              <div className="pt6">
+                <Textarea label="Description" />
+              </div>
+              <div className="pt6">
+                <Toggle label="Enable campaign" />
+              </div>
+            </div>
+          </CreateCampaign.Panel>
+        </div>
       </div>
     )
   }
