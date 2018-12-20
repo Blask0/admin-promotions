e563a947101db84905201b7935e920b30bbc7735
ericreis <eric.figueiredo@vtex.com.br>
2018-12-20 17:40:55 -0200

    Add graphql resolver to fetch collections

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -1,13 +1,14 @@
-import axios from 'axios'
 
 import getBenefits from './resolvers/getBenefits'
 import getCampaigns from './resolvers/getCampaigns'
 import getCategories from './resolvers/getCategories'
+import getCollections from './resolvers/getCollections'
 
 export const resolvers = {
   Query: {
     getBenefits: getBenefits,
     getCampaigns: getCampaigns,
-    getCategories: getCategories
+    getCategories: getCategories,
+    getCollections: getCollections,
   }
 };
