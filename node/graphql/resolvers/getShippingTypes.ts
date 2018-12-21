81f587909cdd12584e0fb16942f411bdfc78aa28
ericreis <eric.figueiredo@vtex.com.br>
2018-12-21 15:26:16 -0200

    Add resolver to fetch shipping types

---
+++ node/graphql/resolvers/getShippingTypes.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { HttpError } from '../errors/httpError'
+
+const getShippingTypes = async (_, info, { vtex: ioContext, request }, query) => {
+  const shippingTypesURL = `http://logistics.vtexcommercestable.com.br/api/logistics/pvt/configuration/freights?an=${
+    ioContext.account
+  }`
+
+  return await axios
+    .get(shippingTypesURL, {
+      headers: {
+        VtexIdclientAutCookie: ioContext.authToken,
+      },
+    })
+    .then(
+      response => (
+        response.data.map(shippingType => ({
+          name: shippingType.name,
+        }))
+      ),
+      error => {
+        const errorMsg = 'Error fetching shipping types'
+        throw new HttpError(errorMsg, error)
+      })
+}
+
+export default getShippingTypes
