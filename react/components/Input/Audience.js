c82bbeaebddc49f749f457670c803500c795ea91
Cauli Tomaz <cauli.tomaz@vtex.com>
2018-12-28 18:12:14 -0200

    Add initial i18n

--- react/components/Input/Audience.js
+++ react/components/Input/Audience.js
@@ -1,25 +1,16 @@
 /* eslint-disable react/jsx-pascal-case */
+/* eslint-disable camelcase */
 import React, { Component } from 'react'
-import MultiSelectWrapper from './MultiSelectWrapper'
-
+import { injectIntl, intlShape } from 'react-intl'
 import translate from '../../graphql/translate.graphql'
-
 import { compose, graphql } from 'react-apollo'
-
-import { EXPERIMENTAL_Conditions } from 'vtex.styleguide'
-
-const isOrNot = [
-  {
-    label: 'is',
-    value: '=',
-    objectId: 'default',
-  },
-  {
-    label: 'is not',
-    value: '!=',
-    objectId: 'default',
-  },
-]
+import {
+  Dropdown,
+  MultiSelect,
+  Input,
+  DatePicker,
+  EXPERIMENTAL_Conditions,
+} from 'vtex.styleguide'
 
 class Audience extends Component {
   constructor(props) {
@@ -30,7 +21,7 @@ class Audience extends Component {
       statements: [],
     }
 
-    const options = {
+    this.options = {
       age: {
         unique: true,
         label: 'User age',
@@ -47,13 +38,13 @@ class Audience extends Component {
           },
         ],
       },
-      color: {
+      shippingMethod: {
         unique: true,
-        label: 'User favorite color',
+        label: props.intl.formatMessage({ id: 'input.label.scheduling' }),
         verbs: [
           {
             label: 'is',
-            value: '=',
+            value: 'is',
             object: this.complexDropdownObject,
           },
           {
@@ -82,20 +73,180 @@ class Audience extends Component {
     }
   }
 
-  render() {
+  complexDropdownObject({ statements, values, statementIndex, error }) {
     return (
-      <EXPERIMENTAL_Conditions
-        showOperator={false}
-        options={options}
-        statements={this.state.statements}
-        operator={this.state.operator}
-        onChangeOperator={this.handleToggleOperator}
-        onChangeStatements={statements => {
+      <Dropdown
+        value={values}
+        options={[]}
+        onChange={(e, value) => {
+          statements[statementIndex].object = value
+
           this.setState({ statements })
         }}
       />
     )
   }
+
+  complexMultiselectObject({ statements, values, statementIndex, error }) {
+    return (
+      <div className="nt3">
+        <MultiSelect
+          emptyState={term => {
+            return `Your search for the color "${term}" did not find any results.`
+          }}
+          options={[]}
+          onChange={selected => {
+            statements[statementIndex].object = selected
+
+            this.setState({ statements })
+          }}
+          selected={values || []}
+        />
+      </div>
+    )
+  }
+
+  complexDatePickerObject({ statements, values, statementIndex, error }) {
+    return (
+      <DatePicker
+        value={values}
+        onChange={date => {
+          statements[statementIndex].object = date
+
+          this.setState({ statements })
+        }}
+        locale="en-US"
+      />
+    )
+  }
+
+  complexDatePickerRangeObject({ statements, values, statementIndex, error }) {
+    return (
+      <div className="flex">
+        <div style={{ maxWidth: 140 }}>
+          <DatePicker
+            style={{ maxWidth: 140 }}
+            value={values && values.from}
+            errorMessage={
+              statements[statementIndex].object &&
+              statements[statementIndex].object.from >=
+                statements[statementIndex].object.to
+                ? 'Must be before end date'
+                : ''
+            }
+            onChange={date => {
+              statements[statementIndex].object = {
+                ...(statements[statementIndex].object || {}),
+                from: date,
+              }
+
+              this.setState({ statements })
+            }}
+            locale="en-US"
+          />
+        </div>
+
+        <div className="mv4 mh3 c-muted-2 b">and</div>
+
+        <div style={{ maxWidth: 140 }}>
+          <DatePicker
+            value={values && values.to}
+            onChange={date => {
+              statements[statementIndex].object = {
+                ...(statements[statementIndex].object || {}),
+                to: date,
+              }
+
+              this.setState({ statements })
+            }}
+            locale="en-US"
+          />
+        </div>
+      </div>
+    )
+  }
+
+  complexNumericInputObject({ statements, values, statementIndex, error }) {
+    return (
+      <Input
+        placeholder="Insert age..."
+        type="number"
+        min={0}
+        value={values}
+        onChange={e => {
+          statements[statementIndex].object = e.target.value.replace(/\D/g, '')
+          this.setState({ statements })
+        }}
+      />
+    )
+  }
+
+  complexNumericInputRangeObject({
+    statements,
+    values,
+    statementIndex,
+    error,
+  }) {
+    return (
+      <div className="flex">
+        <Input
+          placeholder="Age from..."
+          errorMessage={
+            statements[statementIndex].object &&
+            parseInt(statements[statementIndex].object.first) >=
+              parseInt(statements[statementIndex].object.last)
+              ? 'Must be smaller than other input'
+              : ''
+          }
+          value={values && values.first ? values.first : ''}
+          onChange={e => {
+            const currentObject = statements[statementIndex].object || {}
+            currentObject.first = e.target.value.replace(/\D/g, '')
+
+            statements[statementIndex].object = currentObject
+
+            this.setState({ statements })
+          }}
+        />
+
+        <div className="mv4 mh3 c-muted-2 b">and</div>
+
+        <Input
+          placeholder="Age to..."
+          value={values && values.last ? values.last : ''}
+          onChange={e => {
+            const currentObject = statements[statementIndex].object || {}
+            currentObject.last = e.target.value.replace(/\D/g, '')
+
+            statements[statementIndex].object = currentObject
+
+            this.setState({ statements })
+          }}
+        />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <EXPERIMENTAL_Conditions
+          showOperator={false}
+          options={this.options}
+          statements={this.state.statements}
+          operator={this.state.operator}
+          onChangeOperator={this.handleToggleOperator}
+          onChangeStatements={statements => {
+            this.setState({ statements })
+          }}
+        />
+      </div>
+    )
+  }
 }
 
-export default compose(graphql(translate))(Audience)
+Audience.propTypes = {
+  intl: intlShape.isRequired,
+}
+
+export default compose(graphql(translate))(injectIntl(Audience))
