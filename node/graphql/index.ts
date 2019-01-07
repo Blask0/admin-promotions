532e9a9261581afd2bf5ed6532383167e08a43eb
ericreis <eric.figueiredo@vtex.com.br>
2019-01-07 11:59:13 -0200

    Add resolver to fetch promotions

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -1,5 +1,4 @@
 import getAffiliates from './resolvers/getAffiliates'
-import getBenefits from './resolvers/getBenefits'
 import getBrands from "./resolvers/getBrands";
 import getCampaigns from './resolvers/getCampaigns'
 import getCategories from './resolvers/getCategories'
@@ -8,13 +7,13 @@ import getPaymentmethods from './resolvers/getPaymentMethods'
 import getProducts from './resolvers/getProducts'
 import getSalesChannels from "./resolvers/getSalesChannels";
 import getSellers from "./resolvers/getSellers";
+import getPromotions from './resolvers/getPromotions'
 import getShippingTypes from './resolvers/getShippingTypes'
 import getSku from './resolvers/getSku'
 
 export const resolvers = {
   Query: {
     getAffiliates: getAffiliates,
-    getBenefits: getBenefits,
     getBrands: getBrands,
     getCampaigns: getCampaigns,
     getCategories: getCategories,
@@ -23,6 +22,7 @@ export const resolvers = {
     getProducts: getProducts,
     getSalesChannels: getSalesChannels,
     getSellers: getSellers,
+    getPromotions: getPromotions,
     getShippingTypes: getShippingTypes,
     getSku: getSku,
   }
---
+++ node/graphql/resolvers/getPromotions.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { HttpError } from '../errors/httpError'
+
+const getPromotions = async (_, args, { vtex: ioContext }) => {
+  const promotionsURL = `http://${
+    ioContext.account
+  }.vtexcommercestable.com.br/api/rnb/pvt/benefits/calculatorconfiguration`
+  
+  return await axios
+    .get(promotionsURL, {
+      headers: {
+        Authorization: ioContext.authToken,
+      },
+    })
+    .then(
+      response => (
+        response.data.items.map(promotion => ({
+          id: promotion.idCalculatorConfiguration,
+          name: promotion.name,
+          beginDate: promotion.beginDate,
+          endDate: promotion.endDate,
+          isActive: promotion.isActive,
+          description: promotion.description,
+          effectType: promotion.effectType,
+          campaigns: promotion.Campaigns
+        }))
+      ),
+      error => {
+        const errorMsg = 'Error fetching promotions'
+        throw new HttpError(errorMsg, error)
+      })
+}
+
+export default getPromotions
