49c3971cf8f4bd3e298035e841218798bab24ff7
BrunoLerner <bru.lerner@gmail.com>
2019-01-15 13:19:13 -0200

    Get rid of boring js warnings

--- react/components/Icon/Shipping/index.js
+++ react/components/Icon/Shipping/index.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Shipping({ fill = 'none', color = '#111111', size = 32 }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox={`0 0 ${size} ${size}`} fill={fill}>
-      <g clip-path="url(#clip0)">
-        <path d="M12.8976 3H1.89758V25H12.8976V3Z" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"/>
-        <path d="M24.8976 25H31.8976V16L25.8976 14L23.8976 7H12.8976" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"/>
-        <path d="M12.8976 25H16.8976" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"/>
-        <path d="M20.8976 29C23.1067 29 24.8976 27.2091 24.8976 25C24.8976 22.7909 23.1067 21 20.8976 21C18.6884 21 16.8976 22.7909 16.8976 25C16.8976 27.2091 18.6884 29 20.8976 29Z" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"/>
-        <path d="M17.8976 11V14H21.8976" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="square"/>
+      <g clipPath="url(#clip0)">
+        <path d="M12.8976 3H1.89758V25H12.8976V3Z" stroke={color} strokeWidth="2" strokeMiterlimit="10" strokeLinecap="square"/>
+        <path d="M24.8976 25H31.8976V16L25.8976 14L23.8976 7H12.8976" stroke={color} strokeWidth="2" strokeMiterlimit="10" strokeLinecap="square"/>
+        <path d="M12.8976 25H16.8976" stroke={color} strokeWidth="2" strokeMiterlimit="10" strokeLinecap="square"/>
+        <path d="M20.8976 29C23.1067 29 24.8976 27.2091 24.8976 25C24.8976 22.7909 23.1067 21 20.8976 21C18.6884 21 16.8976 22.7909 16.8976 25C16.8976 27.2091 18.6884 29 20.8976 29Z" stroke={color} strokeWidth="2" strokeMiterlimit="10" strokeLinecap="square"/>
+        <path d="M17.8976 11V14H21.8976" stroke={color} strokeWidth="2" strokeMiterlimit="10" strokeLinecap="square"/>
       </g>
       <defs>
         <clipPath id="clip0">
