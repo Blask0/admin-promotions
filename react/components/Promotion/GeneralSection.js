5b29c70bb1798e3d8454916a35913f2988ff4c2c
ericreis <eric.figueiredo@vtex.com.br>
2019-01-15 14:07:37 -0200

    Lint changes in general section component

--- react/components/Promotion/GeneralSection.js
+++ react/components/Promotion/GeneralSection.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape, FormattedMessage } from 'react-intl'
-import {
-    Toggle,
-    Input,
-    DatePicker,
-  } from 'vtex.styleguide'
+import { Toggle, Input, DatePicker } from 'vtex.styleguide'
 
 class GeneralSection extends Component {
   constructor(props) {
@@ -13,73 +9,89 @@ class GeneralSection extends Component {
   }
 
   static contextTypes = {
-    navigate : PropTypes.func,
+    navigate: PropTypes.func,
   }
 
   static propTypes = {
     intl: intlShape,
     generalInfo: PropTypes.object,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
   }
 
   render() {
     const { intl, generalInfo } = this.props
 
     return (
-        <React.Fragment>
-          <h4 className="t-heading-4 mt0">
-            <FormattedMessage id="promotions.promotion.generalInfo.title" />
-          </h4>
-          <div className="mv4">
-            <Input label={intl.formatMessage({ id: "promotions.promotion.generalInfo.status" })}
+      <Fragment>
+        <h4 className="t-heading-4 mt0">
+          <FormattedMessage id="promotions.promotion.generalInfo.title" />
+        </h4>
+        <div className="mv4">
+          <Input
+            label={intl.formatMessage({
+              id: 'promotions.promotion.generalInfo.status',
+            })}
             value={generalInfo.status}
             onChange={e => {
-                this.props.updatePageState({
-                    status: e.target.value
-                })
-            }} />
-          </div>
-          <div className="mv4">
-            <Input label={intl.formatMessage({ id: "promotions.promotion.generalInfo.name" })}
+              this.props.updatePageState({
+                status: e.target.value,
+              })
+            }}
+          />
+        </div>
+        <div className="mv4">
+          <Input
+            label={intl.formatMessage({
+              id: 'promotions.promotion.generalInfo.name',
+            })}
             value={generalInfo.name}
             onChange={e => {
-                this.props.updatePageState({
-                    name: e.target.value
-                })
-            }} />
-          </div>
-          <div className="mv4">
+              this.props.updatePageState({
+                name: e.target.value,
+              })
+            }}
+          />
+        </div>
+        <div className="mv4">
+          <DatePicker
+            locale={intl.locale}
+            onChange={date => {
+              this.props.updatePageState({
+                startDate: date,
+              })
+            }}
+            value={generalInfo.startDate}
+            label={intl.formatMessage({
+              id: 'promotions.promotion.generalInfo.startDate',
+            })}
+          />
+        </div>
+        <Toggle
+          checked={generalInfo.hasEndDate}
+          label={intl.formatMessage({
+            id: 'promotions.promotion.generalInfo.hasEndDate',
+          })}
+          onChange={() => {
+            this.props.updatePageState({ hasEndDate: !generalInfo.hasEndDate })
+          }}
+        />
+        {generalInfo.hasEndDate ? (
+          <div className="mt4">
             <DatePicker
               locale={intl.locale}
               onChange={date => {
                 this.props.updatePageState({
-                    startDate: date
+                  endDate: date,
                 })
               }}
-              value={generalInfo.startDate}
-              label={intl.formatMessage({ id: "promotions.promotion.generalInfo.startDate" })} />
+              value={generalInfo.endDate}
+              label={intl.formatMessage({
+                id: 'promotions.promotion.generalInfo.endDate',
+              })}
+            />
           </div>
-          <Toggle
-            checked={generalInfo.hasEndDate}
-            label={intl.formatMessage({ id: "promotions.promotion.generalInfo.hasEndDate" })}
-            onChange={() => {
-                this.props.updatePageState({hasEndDate: !generalInfo.hasEndDate})
-            }}/>
-          {generalInfo.hasEndDate
-            ? <div className="mt4">
-                <DatePicker
-                  locale={intl.locale}
-                  onChange={date => {
-                    this.props.updatePageState({
-                        endDate: date
-                    })
-                  }}                  
-                  value={generalInfo.endDate}
-                  label={intl.formatMessage({ id: "promotions.promotion.generalInfo.endDate" })} />
-              </div>
-            : null
-          }
-        </React.Fragment>
+        ) : null}
+      </Fragment>
     )
   }
 }
