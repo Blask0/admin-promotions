aad9bf316d7f16b672ee843686ab70ec51d268ac
BrunoLerner <bru.lerner@gmail.com>
2019-01-22 18:02:03 -0200

    Add zip code range statement

--- react/components/Promotion/EligibilitySection.js
+++ react/components/Promotion/EligibilitySection.js
@@ -8,33 +8,12 @@ import {
   shippingMethods,
   paymentMethods,
   utm,
+  zipCodeRange,
 } from '../../utils/conditions/options'
 
 class EligibilitySection extends Component {
   constructor(props) {
     super(props)
-  }
-
-  renderInputObject = ({
-    statements,
-    values,
-    statementIndex,
-    error,
-    extraParams,
-  }) => {
-    const { updatePageState } = this.props
-
-    return (
-      <Input
-        value={values || ''}
-        onChange={e => {
-          statements[statementIndex].object = e.target.value
-          updatePageState({
-            statements: statements,
-          })
-        }}
-      />
-    )
   }
 
   render() {
@@ -49,6 +28,7 @@ class EligibilitySection extends Component {
       paymentMethods: paymentMethods(intl, updatePageState),
       utmSource: utm(intl, updatePageState, 'Source'),
       utmCampaign: utm(intl, updatePageState, 'Campaign'),
+      zipCodeRange: zipCodeRange(intl, updatePageState),
     }
 
     return (
