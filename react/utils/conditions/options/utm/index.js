c663f9391f22ecd081459bd8073464c5750417eb
BrunoLerner <bru.lerner@gmail.com>
2019-01-22 15:00:14 -0200

    add startswith, endswith and any of verbs

--- react/utils/conditions/options/utm/index.js
+++ react/utils/conditions/options/utm/index.js
@@ -1,4 +1,4 @@
-import { renderInputObject } from '../../renders'
+import { renderInputObject, renderSelectObject } from '../../renders'
 
 const utm = (intl, update, type) => {
   const label = `promotions.promotion.elligibility.utm${type}.label`
@@ -21,6 +21,37 @@ const utm = (intl, update, type) => {
       {
         label: 'is not',
         value: '!=',
+        object: {
+          renderFn: renderInputObject,
+          extraParams: {
+            update: update,
+          },
+        },
+      },
+      {
+        // TODO
+        label: 'is any of',
+        value: 'any',
+        object: {
+          renderFn: renderInputObject,
+          extraParams: {
+            update: update,
+          },
+        },
+      },
+      {
+        label: 'starts with',
+        value: 'starts',
+        object: {
+          renderFn: renderInputObject,
+          extraParams: {
+            update: update,
+          },
+        },
+      },
+      {
+        label: 'ends with',
+        value: 'ends',
         object: {
           renderFn: renderInputObject,
           extraParams: {
