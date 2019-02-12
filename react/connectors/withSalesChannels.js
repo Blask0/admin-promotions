d425bf47b183c20199efa7dc361177f7d847faba
BrunoLerner <bru.lerner@gmail.com>
2019-02-12 14:08:14 -0200

    Remove useless constructor

--- react/connectors/withSalesChannels.js
+++ react/connectors/withSalesChannels.js
@@ -5,10 +5,6 @@ import getSalesChannels from '../graphql/getSalesChannels.graphql'
 
 function withSalesChannels(WrappedComponent) {
   class WithSalesChannels extends Component {
-    constructor(props) {
-      super(props)
-    }
-
     render() {
       return (
         <Query query={getSalesChannels}>
