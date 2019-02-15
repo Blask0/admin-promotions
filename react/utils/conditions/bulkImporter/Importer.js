9b7320619bdae2537670417a31782555752bbc45
BrunoLerner <bru.lerner@gmail.com>
2019-02-15 15:00:33 -0200

    Add modal structure

--- react/utils/conditions/bulkImporter/Importer.js
+++ react/utils/conditions/bulkImporter/Importer.js
@@ -2,10 +2,19 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape, FormattedMessage } from 'react-intl'
 import { Button } from 'vtex.styleguide'
+import FileModal from './fileModal'
 
 class Importer extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isImportModalOpen: false,
+    }
+  }
+
+  handleModalToggle = () => {
+    this.setState({ isImportModalOpen: !isImportModalOpen })
   }
 
   render() {
@@ -13,17 +22,33 @@ class Importer extends Component {
 
     return (
       <Fragment>
-        <Button variation="tertiary">Import</Button>
+        <Button variation="tertiary" onClick={this.handleModalToggle}>
+          Import
+        </Button>
+        <ModalDialog
+          centered
+          confirmation={{
+            onClick: this.handleModalToggle,
+            label: 'Ok',
+          }}
+          cancelation={{
+            onClick: this.handleModalToggle,
+            label: 'Cancel',
+          }}
+          isOpen={this.state.isImportModalOpen}
+          onClose={this.handleModalToggle}>
+          <FileModal />
+        </ModalDialog>
       </Fragment>
     )
   }
 }
 
-EligibilitySection.contextTypes = {
+Importer.contextTypes = {
   navigate: PropTypes.func,
 }
 
-EligibilitySection.propTypes = {
+Importer.propTypes = {
   intl: intlShape,
 }
 
