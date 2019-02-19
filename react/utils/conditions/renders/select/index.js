95f477c3db4114300663dfeae8b094dc063d7bbd
BrunoLerner <bru.lerner@gmail.com>
2019-02-19 18:49:19 -0300

    reorganize

--- react/utils/conditions/renders/select/index.js
+++ react/utils/conditions/renders/select/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { EXPERIMENTAL_Select } from 'vtex.styleguide'
+import Importer from '../../../bulkImporter/Importer'
 
 const renderSelectObject = ({
   statements,
@@ -30,31 +31,34 @@ const renderSelectObject = ({
     }
 
     return (
-      <EXPERIMENTAL_Select
-        placeholder={placeholder}
-        options={options}
-        value={selected}
-        multi={multi}
-        ref={statements[statementIndex].refs.object}
-        loading={loading}
-        creatable={creatable}
-        errorMessage={errorMessage}
-        onChange={selected => {
-          if ((isValid && isValid(selected)) || !isValid) {
-            statements[statementIndex].object = selected
-            statements[statementIndex].error = null
-          } else {
-            statements[statementIndex].error = validationErrorMessage
-          }
-          update(statements)
-        }}
-        onSearchInputChange={searchedValue => {
-          updateQueryParams &&
-            updateQueryParams({
-              name: searchedValue,
-            })
-        }}
-      />
+      <Fragment>
+        <EXPERIMENTAL_Select
+          placeholder={placeholder}
+          options={options}
+          value={selected}
+          multi={multi}
+          ref={statements[statementIndex].refs.object}
+          loading={loading}
+          creatable={creatable}
+          errorMessage={errorMessage}
+          onChange={selected => {
+            if ((isValid && isValid(selected)) || !isValid) {
+              statements[statementIndex].object = selected
+              statements[statementIndex].error = null
+            } else {
+              statements[statementIndex].error = validationErrorMessage
+            }
+            update(statements)
+          }}
+          onSearchInputChange={searchedValue => {
+            updateQueryParams &&
+              updateQueryParams({
+                name: searchedValue,
+              })
+          }}
+        />
+        <Importer />
+      </Fragment>
     )
   })
 
