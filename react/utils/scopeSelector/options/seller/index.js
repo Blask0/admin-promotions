365cffeb01ee32cdd0f357f8fbd9bed3265873ac
BrunoLerner <bru.lerner@gmail.com>
2019-02-26 15:37:14 -0300

    Add seller statement to scope selector

---
+++ react/utils/scopeSelector/options/seller/index.js
@@ -0,0 +1,61 @@
+import { renderSelectObject } from '../../renders'
+
+import withSellers from '../../../../connectors/withSellers'
+
+const mapSellers = sellers =>
+  sellers.map(seller => ({
+    label: seller.name,
+    value: seller.id,
+  }))
+
+const seller = (intl, update) => {
+  return {
+    label: intl.formatMessage({
+      id: 'promotions.promotion.scopeSelector.seller.label',
+    }),
+    verbs: [
+      {
+        label: intl.formatMessage({
+          id: 'promotions.promotion.scopeSelector.seller.verb.any',
+        }),
+        value: 'any',
+        object: {
+          renderFn: renderSelectObject,
+          extraParams: {
+            queryInfo: {
+              connector: withSellers,
+              dataGetter: ({ sellers = [] }) => mapSellers(sellers),
+            },
+            placeholder: intl.formatMessage({
+              id: 'promotions.promotion.scopeSelector.seller.placeholder',
+            }),
+            multi: true,
+            update: update,
+          },
+        },
+      },
+      {
+        label: intl.formatMessage({
+          id: 'promotions.promotion.scopeSelector.seller.verb.not.any',
+        }),
+        value: 'not.any',
+        object: {
+          renderFn: renderSelectObject,
+          extraParams: {
+            queryInfo: {
+              connector: withSellers,
+              dataGetter: ({ sellers = [] }) => mapSellers(sellers),
+            },
+            placeholder: intl.formatMessage({
+              id: 'promotions.promotion.scopeSelector.seller.placeholder',
+            }),
+            multi: true,
+            update: update,
+          },
+        },
+      },
+    ],
+  }
+}
+
+export default seller
