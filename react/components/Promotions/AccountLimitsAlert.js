a1522f6f981494a6b8e15c3ea3971c527c5bdfa1
ericreis <eric.figueiredo@vtex.com.br>
2019-04-12 19:08:00 -0300

    Add alert if account has reached promotions limit (or is close to it)

---
+++ react/components/Promotions/AccountLimitsAlert.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { injectIntl, intlShape, FormattedMessage } from 'react-intl'
+
+import { Alert } from 'vtex.styleguide'
+
+const ref = React.createRef()
+
+function AccountLimitsAlert({ intl, promotions, accountLimits }) {
+  const activePromotions = promotions.filter(({ isActive }) => isActive)
+  const percentual = activePromotions.length / accountLimits.activePromotions
+
+  const alertProps =
+    percentual >= 1
+      ? {
+        type: 'error',
+        message: 'promotions.promotion.accountLimit.exceeded',
+      }
+      : percentual > 0.9
+        ? {
+          type: 'warning',
+          message: 'promotions.promotion.accountLimit.warning',
+        }
+        : null
+
+  return (
+    alertProps && (
+      <Alert ref={ref} type={alertProps.type}>
+        {/* action={{
+          label: intl.formatMessage({
+            id: 'promotions.promotion.accountLimit.moreInfo',
+          }),
+          onClick: () => {},
+        }} */}
+        <FormattedMessage
+          id={alertProps.message}
+          values={{
+            current: activePromotions.length,
+            limit: accountLimits.activePromotions,
+          }}
+        />
+      </Alert>
+    )
+  )
+}
+
+AccountLimitsAlert.propTypes = {
+  intl: intlShape,
+  promotions: PropTypes.arrayOf(PropTypes.object),
+  accountLimits: PropTypes.shape({
+    activePromotions: PropTypes.number,
+  }).isRequired,
+}
+
+export default injectIntl(AccountLimitsAlert)
