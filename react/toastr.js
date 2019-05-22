255dea9119596abb07af065cacac35c6260024d7
ericreis <eric.figueiredo@vtex.com.br>
2019-05-22 19:21:20 -0300

    Add global css to make content occupy full height

--- react/toastr.js
+++ react/toastr.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { ToastProvider } from 'vtex.styleguide'
 
+import './styles.global.css'
+
 export default function({ children }) {
   return <ToastProvider positioning="window">{children}</ToastProvider>
 }
