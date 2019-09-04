07ca0a0770c9da3fa8393e00df17558edac55e8e
ericreis <eric.figueiredo@vtex.com.br>
2019-09-04 14:19:35 -0300

    Delete legacy conditions renders

--- react/utils/scopeSelector/options/index.js
+++
@@ -1,8 +0,0 @@
-import brand from './brand'
-import category from './category'
-import collection from './collection'
-import product from './product'
-import seller from './seller'
-import sku from './SKU'
-
-export { brand, category, collection, product, seller, sku }
